1443bb9472b52ae5f3c47d4c27a1a3e379569d19
gabrielbucalon <bucalongabriell@gmail.com>
2020-05-16 16:26:46 -0300

    feature | consuming finance API and added 'appBar'

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=5b236068";
+
+void main() async{
+
+  print(await getData());
+
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
+
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+
+  return json.decode(response.body);
+}
